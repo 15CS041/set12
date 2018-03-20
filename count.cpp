8cbeee10e1b185cd6c925367dd592a9a97ad7941
15CS041 <rajaudhayaap1998@gmail.com>
2018-03-20 15:57:23 +0530

    Create count.cpp

---
+++ count.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+using namespace std;
+int main();
+{
+int count=0,num;
+cout<<"Enter the value:";
+cin>>num;
+while (num>0)
+{
+num=num/10;
+count++
+}
+cout<<"print the count value:";
+}
